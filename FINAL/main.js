2d3de5d4f3f1ce5f87ae2e4863eec1e2045eaab6
Diogo-Siqueira <126920478+Diogo-Siqueira@users.noreply.github.com>
2023-05-04 15:05:19 -0300

    Add files via upload

---
+++ FINAL/main.js
@@ -0,0 +1,7 @@
+window.onkeydown = function final(tecla) {
+    if (tecla.key == "1") {
+        window.location.href = "../MENU/menu.html"
+    } else if (tecla.key == "3") {
+        window.location.href = 'https://www.google.com'
+    }
+}
