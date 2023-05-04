a4f25e565be81b40e18fafe707c1e295e3d51576
Diogo-Siqueira <126920478+Diogo-Siqueira@users.noreply.github.com>
2023-05-04 20:44:21 -0300

    Update main.js

--- TUTORIAL/tutorial3/main.js
+++ TUTORIAL/tutorial3/main.js
@@ -14,20 +14,20 @@ renderizarMapa(alturaMapa, larguraMapa)
 
 window.onkeydown = function andar(tecla){
     if(tecla.key == "w"){
-        if(mapa[coordPlayer[0] - 1][coordPlayer[1]] != "*" && mapa[coordPlayer[0] - 1][coordPlayer[1]] != "H"){
+        if(mapa[coordPlayer[0] - 1][coordPlayer[1]] != "*" && mapa[coordPlayer[0] - 1][coordPlayer[1]] != "H" && mapa[coordPlayer[0] - 1][coordPlayer[1]] != "W"){
             coordPlayer[0]--
         }
     }else if(tecla.key == "a"){
-        if(mapa[coordPlayer[0]][coordPlayer[1] - 1] != "*" && mapa[coordPlayer[0]][coordPlayer[1] - 1] != "H"){
+        if(mapa[coordPlayer[0]][coordPlayer[1] - 1] != "*" && mapa[coordPlayer[0]][coordPlayer[1] - 1] != "H" && mapa[coordPlayer[0]][coordPlayer[1] - 1] != "W"){
             coordPlayer[1]--
         }
     }else if(tecla.key == "s"){
-        if(mapa[coordPlayer[0] + 1][coordPlayer[1]] != "*" && mapa[coordPlayer[0] + 1][coordPlayer[1]] != "H"){
+        if(mapa[coordPlayer[0] + 1][coordPlayer[1]] != "*" && mapa[coordPlayer[0] + 1][coordPlayer[1]] != "H" && mapa[coordPlayer[0] + 1][coordPlayer[1]] != "W"){
             coordPlayer[0]++
         }
 
     }else if(tecla.key == "d"){
-        if(mapa[coordPlayer[0]][coordPlayer[1] + 1] != "*" && mapa[coordPlayer[0]][coordPlayer[1] + 1] != "H"){
+        if(mapa[coordPlayer[0]][coordPlayer[1] + 1] != "*" && mapa[coordPlayer[0]][coordPlayer[1] + 1] != "H" && mapa[coordPlayer[0]][coordPlayer[1] + 1] != "W"){
             coordPlayer[1]++        
         }
     }else if(tecla.key == "e"){
