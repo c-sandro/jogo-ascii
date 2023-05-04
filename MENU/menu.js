3094ff5577bb8ce9c4e230f34f2cb0b02091dc6a
Diogo-Siqueira <126920478+Diogo-Siqueira@users.noreply.github.com>
2023-05-03 22:09:45 -0300

    Update menu.js

--- MENU/menu.js
+++ MENU/menu.js
@@ -1,8 +1,8 @@
 window.onkeydown = function jogar(tecla) {
     if (tecla.key == "1") {
-        window.location.href = "../beta/index.html"
+        window.location.href = "../beta/index.html"     //ajustar de acordo com a localização da pasta do jogo
     } else if (tecla.key == "2") {
-        window.location.href = "../tutorial/index.html"
+        window.location.href = "../tutorial/index.html" //ajustar de acordo com a localização da pasta do tutorial
     } else if (tecla.key == "3"){
         window.close()
     }
