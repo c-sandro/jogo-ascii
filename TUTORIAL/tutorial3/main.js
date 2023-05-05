458fc893693a8ef510135cef595e0f9928608d3e
c-sandro <124182880+c-sandro@users.noreply.github.com>
2023-05-05 10:37:28 -0300

    Update main.js

--- TUTORIAL/tutorial3/main.js
+++ TUTORIAL/tutorial3/main.js
@@ -1,67 +1,61 @@
 var mapa = [["*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*"],
-            ["*", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "H", " ", "*"],
-            ["*", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "W", " ", "*"],
-            ["*", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "W", " ", "*"],
-            ["*", " ", " ", " ", " ", " ", " ", " ", " ", "k", " ", " ", "W", " ", "U"],
-            ["*", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "W", " ", "*"],
-            ["*", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "W", " ", "*"],
-            ["*", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "W", " ", "*"],
+            ["*", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "M", " ", "*"],
+            ["*", " ", " ", " ", " ", " ", " ", " ", " ", " ", "M", " ", "M", " ", "*"],
+            ["*", " ", " ", " ", " ", " ", " ", " ", " ", " ", "M", " ", "M", " ", "*"],
+            ["*", " ", " ", " ", " ", " ", " ", " ", " ", " ", "M", " ", "M", " ", "U"],
+            ["*", " ", " ", " ", " ", " ", " ", " ", " ", " ", "M", " ", "M", " ", "*"],
+            ["*", " ", " ", " ", " ", " ", " ", " ", " ", " ", "M", " ", "M", " ", "*"],
+            ["*", " ", " ", " ", " ", " ", " ", " ", " ", " ", "M", " ", " ", " ", "*"],
             ["*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*"]]
 var coordPlayer = [4,2]
 var textTela = document.getElementById("textTela")  
-var larguraMapa = 15, alturaMapa = 9
-renderizarMapa(alturaMapa, larguraMapa)
+var tamY = 9, tamX = 15
+var fim = false
+renderizarMapa(tamY, tamX)
 
 window.onkeydown = function andar(tecla){
     if(tecla.key == "w"){
-        if(mapa[coordPlayer[0] - 1][coordPlayer[1]] != "*" && mapa[coordPlayer[0] - 1][coordPlayer[1]] != "H" && mapa[coordPlayer[0] - 1][coordPlayer[1]] != "W"){
+        if(mapa[coordPlayer[0] - 1][coordPlayer[1]] != "*" && mapa[coordPlayer[0] - 1][coordPlayer[1]] != "H"){
             coordPlayer[0]--
         }
     }else if(tecla.key == "a"){
-        if(mapa[coordPlayer[0]][coordPlayer[1] - 1] != "*" && mapa[coordPlayer[0]][coordPlayer[1] - 1] != "H" && mapa[coordPlayer[0]][coordPlayer[1] - 1] != "W"){
+        if(mapa[coordPlayer[0]][coordPlayer[1] - 1] != "*" && mapa[coordPlayer[0]][coordPlayer[1] - 1] != "H"){
             coordPlayer[1]--
         }
     }else if(tecla.key == "s"){
-        if(mapa[coordPlayer[0] + 1][coordPlayer[1]] != "*" && mapa[coordPlayer[0] + 1][coordPlayer[1]] != "H" && mapa[coordPlayer[0] + 1][coordPlayer[1]] != "W"){
+        if(mapa[coordPlayer[0] + 1][coordPlayer[1]] != "*" && mapa[coordPlayer[0] + 1][coordPlayer[1]] != "H"){
             coordPlayer[0]++
         }
-
     }else if(tecla.key == "d"){
-        if(mapa[coordPlayer[0]][coordPlayer[1] + 1] != "*" && mapa[coordPlayer[0]][coordPlayer[1] + 1] != "H" && mapa[coordPlayer[0]][coordPlayer[1] + 1] != "W"){
+        if(mapa[coordPlayer[0]][coordPlayer[1] + 1] != "*" && mapa[coordPlayer[0]][coordPlayer[1] + 1] != "H"){
             coordPlayer[1]++        
         }
-    }else if(tecla.key == "e"){
-        if(mapa[coordPlayer[0]][coordPlayer[1]] == "k"){
-            for(var i = 0; i < alturaMapa; i++){
-                for(var j = 0; j < larguraMapa; j++){
-                    if(mapa[i][j] == "H"){
-                        mapa[i][j] = "U"
-                    }
-                }
-            }
-            mapa[coordPlayer[0]][coordPlayer[1]] = " "
+    }else if (tecla.key == "3"){
+        if(fim == true){
+            window.location.href = "../../menu/index.html"            
         }
-
     }
-    if (coordPlayer[0] == 4 && coordPlayer[1] == 14) {
-        document.getElementById("instrucao").innerHTML = `Você completou o Tutorial, parabéns!.<br>Aperte 3 para voltar ao menu`
+    if(mapa[coordPlayer[0]][coordPlayer[1]] == "M"){
+        coordPlayer = [4,2]
     }
-    if (tecla.key == "3"){
-        window.location.href = "/MENU/menu.html"
+    if(coordPlayer[0] == 4 && coordPlayer[1] == 14) {
+        document.getElementById("instrucao").innerHTML = "Você completou o Tutorial, parabéns!. <br>"
+        document.getElementById("instrucao").innerText += "Aperte 3 para voltar ao menu"
+        fim = true
     }
-    renderizarMapa(alturaMapa,larguraMapa)
+    renderizarMapa(tamY, tamX)
 }
 
-function renderizarMapa() {
-    textTela.innerText = "";
-    for (var i = 0; i < mapa.length; i++) {
-      for (var j = 0; j < mapa[i].length; j++) {
-        if (coordPlayer[0] == i && coordPlayer[1] == j) {
-          textTela.innerText += " " + "&";
-        } else {
-          textTela.innerText += " " + mapa[i][j];
+function renderizarMapa(y, x){
+    textTela.innerText = ""
+    for (var i = 0; i < y; i++) {
+        for (var j = 0; j < x; j++) {
+            if (coordPlayer[0] == i && coordPlayer[1] == j) {
+                textTela.innerText += " &"
+            }else{
+                textTela.innerText += " " + mapa[i][j]
+            }
         }
-      }
-      textTela.innerHTML += "<br>";
-    }
-  }
+        textTela.innerHTML += "<br>"
+        }
+}
